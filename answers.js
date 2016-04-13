2d50f7d4579fa50e03e8cefe64d6bf2cf2c91e85
carmholm <ameliacarmenholmes@gmail.com>
2016-04-13 17:33:02 +0000

    Exercise 2 completed

--- answers.js
+++ answers.js
@@ -6,3 +6,10 @@ function firstCharacter (string) {
    return(string.substring(0,1));
 };
 
+// This function takes a string and returns the last character of a string
+
+function lastCharacter (string) {
+    var stringLength = string.length; 
+    var lastChar = string.charAt(stringLength - 1); 
+    return(lastChar);
+};
