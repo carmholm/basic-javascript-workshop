481276ef41f425a5322038a21d01cd7a4334ac54
carmholm <ameliacarmenholmes@gmail.com>
2016-04-13 18:16:36 +0000

    Exercise 6 completed

--- answers.js
+++ answers.js
@@ -33,3 +33,28 @@ function sum (num1, num2){
 function product (num1, num2){
     return(num1*num2);
 }
+
+//This function takes two numbers and a string. If the string is ‘add’, it returns the sum of the numbers. If the string is ‘subtract’, it returns the difference. If the string is ‘mult’, it returns the product. If the string is ‘div’, it returns the ratio. Otherwise it returns 0.
+
+function exerciseSix (num1, num2, string){
+    if (string === "add"){
+        return(num1 + num2);
+    }
+    else if (string === "subtract"){
+        if (num1 > num2){
+            return(num1 - num2)
+        }
+        else {
+        return(num2 - num1);
+        }
+    }
+    else if (string === "mult"){
+        return(num1*num2);
+    }
+    else if (string === "div"){
+        return(num1/num2);
+    }
+    else {
+        return(0);
+    }
+}
